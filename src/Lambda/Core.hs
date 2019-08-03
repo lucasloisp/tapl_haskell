e4b7117936f48161dc664aab9771e42a6dbdba45
Lucas Lois <lucasloisp@gmail.com>
2019-08-03 15:15:13 -0300

    Implements termShift

--- src/Lambda/Core.hs
+++ src/Lambda/Core.hs
@@ -3,4 +3,7 @@ module Lambda.Core (termShift) where
 import Lambda.Syntax
 
 termShift :: Int -> Term -> Term
-termShift d t = t
+termShift d t = walk 0 t
+  where walk c (TmVar x) = TmVar (if x < c then x else x+d)
+        walk c (TmAbs t1) = TmAbs (walk (c+1) t1)
+        walk c (TmApp t1 t2) = TmApp (walk c t1) (walk c t2)
--- test/LambdaSpec.hs
+++ test/LambdaSpec.hs
@@ -2,11 +2,23 @@ module LambdaSpec (spec) where
 
 import Test.Hspec
 import Control.Exception (evaluate)
+import Test.QuickCheck
 import Lambda.Core
 import Lambda.Syntax
 
 spec :: Spec
 spec =  do
   describe "Lambda.Core.termShift" $ do
-    it "is the identity when shifting by 0" $ termShift 0 (TmVar 0)  `shouldBe` (TmVar 0)
+    it "is the does not change variables when shifting by 0" $ property $
+      \i -> termShift 0 (TmVar i) == (TmVar i)
+    it "correctly does not shift a bound variable" $ property $
+      \i -> termShift (getPositive i) (TmAbs (TmVar 0)) == (TmAbs (TmVar 0))
+    it "shifts a free variable in an abstraction" $ property $
+      \d ->
+      \f ->
+        termShift (getPositive d) (TmAbs (TmVar (getPositive f))) ==
+	(TmAbs (TmVar ((getPositive f) + (getPositive d))))
+    it "Shifts both terms in an application" $
+      termShift 2 (TmApp (TmAbs (TmApp (TmVar 0) (TmVar 1))) (TmVar 4)) `shouldBe`
+      (TmApp (TmAbs (TmApp (TmVar 0) (TmVar 3))) (TmVar 6))
 
